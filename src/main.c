cbce0a55f5637d9f11be0c3b61cd5e248e14644b
Otto Hanski <otto.hanski@gmail.com>
2024-01-07 22:56:26 +0200

    Test

---
+++ src/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+
+#define MAX_CLIENTS 5
+
+int main(){
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in, serv_addr, cli_addr;
+    int n, i, max_sd, sd;
+    int client_socket[MAX_CLIENTS];
+    fd_set readfds;
+
+    for (i = 0; i < MAX_CLIENTS; i++) {
+        client_socket[i] = 0;
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = 8080;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    listen(sockfd, MAX_CLIENTS);
+    clilen = sizeof(cli_addr);
+
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        max_sd = sockfd;
+
+        for (i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if (sd > max_sd) {
+                max_sd = sd;
+            }
+        }
+
+        select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if (FD_ISSET(sockfd, &readfds)) {
+            newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+            if (newsockfd < 0) {
+                perror("ERROR on accept");
+                exit(1);
+            }
+
+            for (i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = newsockfd;
+                    break;
+                }
+            }
+        }
+
+        for (i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+            if (FD_ISSET(sd, &readfds)) {
+                bzero(buffer,256);
+                n = read(sd, buffer, 255);
+                if (n < 0) {
+                    perror("ERROR reading from socket");
+                    exit(1);
+                }
+                printf("Here is the message: %s\n", buffer);
+            }
+        }
+    }
+
+    close(sockfd);
+
+    return 0;
+}
